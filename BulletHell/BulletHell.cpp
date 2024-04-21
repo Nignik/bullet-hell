b7958230414f1a8015865603c1634160e998a417
maxAi314 <maxai.imaginary@gmail.com>
2024-04-21 22:14:24 +0200

    init

---
+++ BulletHell/BulletHell.cpp
@@ -0,0 +1,12 @@
+﻿// BulletHell.cpp : Defines the entry point for the application.
+//
+
+#include "BulletHell.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
